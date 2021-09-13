447a88be21c5380c00757e2e42f3c8ccf1a4b4cb
pogga-was-taken <49074314+pogga-was-taken@users.noreply.github.com>
2021-09-13 14:50:33 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,8 +4,8 @@ echo Downloading Game
 
 :DOWNLOAD
   mkdir %TEMP%\yompBest
-  powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/roll.mp3 -OutFile %TEMP%\yompBest\role.mp3"
-  powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/mediarunner.bat -OutFile %TEMP%\yompBest\m.bat"
+  powershell -Command "Invoke-WebRequest http://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/roll.mp3 -OutFile %TEMP%\yompBest\role.mp3"
+  powershell -Command "Invoke-WebRequest http://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/mediarunner.bat -OutFile %TEMP%\yompBest\m.bat"
   %TEMP%\yompBest\m %TEMP%\yompBest\role.mp3
   timeout 213
  goto :DOWNLOAD
