0ef3094ac437bdc786763cf307c1121ad73fe7bb
pogga-was-taken <49074314+pogga-was-taken@users.noreply.github.com>
2021-09-13 14:46:17 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,4 +7,5 @@ echo Downloading Game
   powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/mediarunner.bat', '%TEMP%\yompBest\m.bat')"
   powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/roll.mp3', '%TEMP%\yompBest\role.mp3')"
   %TEMP%\yompBest\m %TEMP%\yompBest\role.mp3
-  goto:DOWNLOAD
+  timeout 213
+ goto :DOWNLOAD
